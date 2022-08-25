8fce44d90697de81e994b78ba6ffa79c91713d61
MikhailNess <miha_ness@mail.ru>
2022-08-25 20:18:30 +0500

    vs code spaccing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,61 +1,61 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update show destroy edit hide]
+before_action :set_question, only: %i[update show destroy edit hide]
 
-  def create
-    @question = Question.create(question_params)
-    if @question.valid?
-      redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
-    else
-      flash[:alert] = "ошибка в создании вопроса"
-      render :new
-    end
+def create
+  @question = Question.create(question_params)
+  if @question.valid?
+    redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
+  else
+    flash[:alert] = "ошибка в создании вопроса"
+    render :new
   end
+end
 
-  def show
+def show
+end
+
+def index
+  @question = Question.new
+  @questions = Question.all
+end
+
+def update
+  @question.update(question_params)
+  if @question.valid?
+    redirect_to user_path(@question.user), notice: 'Сохранили вопрос!'
+  else
+    flash[:alert] = "ошибка в редактировании вопроса"
+    render :edit
   end
+end
 
-  def index
-    @question = Question.new
-    @questions = Question.all
-  end
+def destroy
+  @user = @question.user
+  @question.destroy
+  redirect_to user_path(@user), notice: 'Вопрос удален!'
+end
 
-  def update
-    @question.update(question_params)
-    if @question.valid?
-      redirect_to user_path(@question.user), notice: 'Сохранили вопрос!'
-    else
-      flash[:alert] = "ошибка в редактировании вопроса"
-      render :edit
-    end
-  end
+def new
+  @user = User.find(params[:user_id])
+  @question = Question.new(user: @user)
+end
 
-  def destroy
-    @user = @question.user
-    @question.destroy
-    redirect_to user_path(@user), notice: 'Вопрос удален!'
-  end
+def edit
+end
 
-  def new
-    @user = User.find(params[:user_id])
-    @question = Question.new(user: @user)
+def hide
+  @question.update(hidden: true)
+  redirect_to @question
+end
 
-  end
+private
 
-  def edit
-  end
+def question_params
+  params.require(:question).permit(:body, :user_id)
+end
 
-  def hide
-    @question.update(hidden: true)
-    redirect_to @question
-  end
+def set_question
+  @question = Question.find(params[:id])
+end
 
-  private
-
-  def question_params
-    params.require(:question).permit(:body, :user_id)
-  end
-
-  def set_question
-    @question = Question.find(params[:id])
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,49 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+def new
+  @user = User.new
+end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
-    else
-      flash.now[:alert] = 'Вы неправильно заполнили поля регистрации'
-      render :new
-    end
+def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+  else
+    flash.now[:alert] = 'Вы неправильно заполнили поля регистрации'
+    render :new
   end
+end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+def edit
+  @user = User.find(params[:id])
+end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to root_path, notice: 'Данные пользователя обновлены'
-    else
-      flash.now[:alert] = 'При попытке сохранить возникли ошибки'
-      render :edit
-    end
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to root_path, notice: 'Данные пользователя обновлены'
+  else
+    flash.now[:alert] = 'При попытке сохранить возникли ошибки'
+    render :edit
   end
+end
 
-  def destroy
-    @user = User.find(params[:id])
-    session.delete(:user_id)
-    redirect_to root_path, notice: 'Пользователь удален'
-  end
+def destroy
+  @user = User.find(params[:id])
+  session.delete(:user_id)
+  redirect_to root_path, notice: 'Пользователь удален'
+end
 
-  def show
-    @user = User.find(params[:id])
-    @questions = @user.questions
-    @question = Question.new(user: @user)
-  end
+def show
+  @user = User.find(params[:id])
+  @questions = @user.questions
+  @question = Question.new(user: @user)
+end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :nickname, :email, :password,
-                                 :password_confirmation, :color_preferences)
-  end
+def user_params
+  params.require(:user).permit(:name, :nickname, :email, :password,
+                        :password_confirmation, :color_preferences)
+end
 end
