091375cae7c79f043d8881c41db7b4ee55cc31a6
MikhailNess <miha_ness@mail.ru>
2022-08-26 23:09:35 +0500

    validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-  validates :body, presence: true, length:{maximum: 280}
+  validates :body, presence: true, length:{ maximum: 280 } 
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
-  before_validation :downcase_nickname
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
-  validates :nickname, uniqueness: true, format: {with: /[a-z+0-9_]/}, length: {maximum: 40}
-  validates :color_preferences, presence: true
-
   has_many :questions, dependent: :delete_all
 
-  def downcase_nickname
-    nickname.downcase!
-  end
+  before_validation :downcase_nickname
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+  validates :nickname, uniqueness: true, format: {with: /\A[a-zA-Z0-9]+\Z/}, length: {maximum: 40}
+  validates :color_preferences, presence: true
 
+private 
+
+  def downcase_nickname
+    nickname&.downcase!
+  end
 end
